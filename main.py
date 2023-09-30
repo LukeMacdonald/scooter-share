5cda31ee23a44853cb07a323cc1ee05ac50ea7a8
LukeMacdonald <lukemacdonald21@gmail.com>
2023-09-30 18:23:04 +1000

    changed agent flask app to run on https to allow tracking user current location

--- main.py
+++ main.py
@@ -14,8 +14,7 @@ def run_master(master):
 def run_agent():
     "Function to run the Flask agent application."
     agent = create_agent_app()
-    agent.run(host=socket_utils.PUBLIC_HOST, port=socket_utils.AGENT_PORT, debug=False, threaded=True)
-                    
+    agent.run(host=socket_utils.PUBLIC_HOST, ssl_context=('cert.pem', 'key.pem'),port=socket_utils.AGENT_PORT, debug=False, threaded=True)               
 if __name__ == '__main__':
     app = create_master_app()
     master_thread = threading.Thread(target=run_master, args=(app,))
