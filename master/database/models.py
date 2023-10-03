1374d3e9c50b737b33c075b9daa622319b4d264f
Damon O'Malley <s3909845@student.rmit.edu.au>
2023-10-03 11:53:25 +1100

    Add phone_number to User.as_json

--- master/database/models.py
+++ master/database/models.py
@@ -65,6 +65,7 @@ class User(db.Model):
             'first_name': self.first_name,
             'last_name': self.last_name,
             'role': self.role,
+            'phone_number': self.phone_number,
             'balance': self.balance
         }
 
