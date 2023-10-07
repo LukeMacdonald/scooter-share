21081b096741e20daca9164b59f5d1af2e0f5688
Bailey Vogt <s3906263@student.rmit.edu.au>
2023-10-07 07:42:17 +0100

    writing database model for faces and moving recognition code

--- agent/scooter/facial_recognition/facial_recognition.py
+++ agent/scooter/facial_recognition/facial_recognition.py
@@ -6,7 +6,6 @@ import face_recognition
 import imutils
 from datetime import datetime
 from imutils.video import VideoStream
-import database.models
 
 
 
@@ -30,13 +29,15 @@ class facial_recognition():
             return 1
 
 
-        face_cascade = cv2.CascadeClassifier('facial_recognition/haarcascade_frontalface_default.xml')
+        face_cascade = cv2.CascadeClassifier('agent/scooter/facial_recognition/haarcascade_frontalface_default.xml')
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         face = face_cascade.detectMultiScale(gray, 1.3, 5)
 
         if len(face) == 0:
             print(f'No face detected, restarting')
+            self.get_photo()
             exit()
+        
         print('got face')
         
         for x, y, w, h in face:
@@ -48,14 +49,14 @@ class facial_recognition():
         self.encode_photo()
 
     def encode_photo(self):
+        print('encoding')
 
         rgb = cv2.cvtColor(self.image, cv2.COLOR_BGR2RGB)
-        boxes = face_recognition.face_locations()
+        boxes = face_recognition.face_locations(self.image)
         encodings = face_recognition.face_encodings(rgb, boxes)
 
         for encoding in encodings:
-            self.add_encoding(encoding, self.user)
-            print(f'user: {self.user}, encoding: {encoding}')
+            self.add_encoding(encoding)
 
 
     def recognize(self):
@@ -88,17 +89,17 @@ class facial_recognition():
                     return max(counts, key=counts.get)
 
 
-    def add_encodings(self, e):
+    def add_encoding(self, e):
         '''
             add user and encoding data to database
         '''
-        pass
+        print(e)
 
     def get_encodings(self):
         '''
             get user and encoding data from database
         '''
-        pass
+        
 
 if __name__ == '__main__':
     recognizer = facial_recognition('name')
